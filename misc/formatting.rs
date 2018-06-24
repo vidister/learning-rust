41539724a57079c71f7b8e2626fd53c9a2c3e06b
vidister <git@vidister.de>
2018-06-24 19:17:08 +0200

    add misc learning files

---
+++ misc/formatting.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let distance = 100i8; // assign the 8 bit signed integer 100 to the immutable variable distance.
+
+    // this is a single line comment
+
+    // println! is a macro. that prints the formated string to stdout
+    println!("You are {} miles away.", distance);
+}
---
+++ misc/hello-world.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // println! is a macro that prints a string to stdout. For now you can just
+    // think of macros as being the same as functions.
+    println!("Hello, World");
+}
